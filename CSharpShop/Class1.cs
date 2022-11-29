e4927a9379cb4535c9e77054c3aa6f5d6d809b42
GioPa97 <giovannipagliara97@.com>
2022-11-29 15:24:45 +0100

    Creata classe, getter e setter

---
+++ CSharpShop/Class1.cs
@@ -0,0 +1,54 @@
+﻿namespace CSharpShop
+{
+    public class prodotto
+    {
+        private int codice;
+        private string nome;
+        private string descrizione;
+        private int prezzo;
+        private int iva;
+
+        //GETTER//
+        public int getCodice()
+        {
+            return this.codice;
+        }
+        public string getNome()
+        {
+            return this.nome;
+        }
+        public string getDescrizione()
+        {
+            return this.descrizione;
+        }
+        public int getPrezzo()
+        {
+            return this.prezzo;
+        }
+        public int getIva()
+        {
+            return this.iva;
+        }
+        //SETTER//
+        public void setCodice(int codice)
+        {
+            this.codice = codice;
+        }
+        public void setNome(string nome)
+        {
+            this.nome = nome;
+        }
+        public void setDescrizione(string descrizione)
+        {
+            this.descrizione = descrizione;
+        }
+        public void setPrezzo(int prezzo)
+        {
+            this.prezzo = prezzo;
+        }
+        public void setIva(int iva)
+        {
+            this.iva = iva;
+        }
+    }
+}
