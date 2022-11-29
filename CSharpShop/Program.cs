47465f153ff5e8d219dbde147afc305a9885a7c8
GioPa97 <giovannipagliara97@.com>
2022-11-29 15:10:57 +0100

    Aggiungere i file di progetto.

---
+++ CSharpShop/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Nel progetto CSharpShop , creare la classe Prodotto che gestisce i prodotti del vostro shop.
+Un prodotto è caratterizzato da:
+codice (numero intero)
+nome
+descrizione
+prezzo
+iva
+Usate opportunamente i livelli di accesso (public, private), i costruttori, i metodi getter e setter ed eventuali altri metodi di “utilità” per fare in modo che:
+alla creazione di un nuovo prodotto il codice sia valorizzato con un numero random.
+Il codice prodotto sia accessibile solo in lettura
+Gli altri attributi siano accessibili sia in lettura che in scrittura
+Il prodotto esponga sia un metodo per avere il prezzo base che uno per avere il prezzo comprensivo di iva.
+Il prodotto esponga un metodo per avere il nome esteso, ottenuto concatenando codice + nome
+Testate poi i vostri oggetti Prodotto, istanziandoli e provando ad interagirci con loro per testare tutti i metodi che avete previsto e implementato.
+BONUS:
+create un metodo che restituisca il codice con un pad left di 0 per arrivare a 8 caratteri (ad esempio codice 91 diventa 00000091, mentre codice 123445567 resta come è)
+Usando una List, dichiarate una lista dei prodotti di un negozio e inseriteci al suo interno qualche prodotto che vi aspettate di trovare nel negozio. Stampate poi l’elenco dei vostri prodotti che avete previsto nel negozio.*/
+
